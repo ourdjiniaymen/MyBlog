b32502356cbf7f110d089abb5cc9eb0e630746db
ourdjini aymen <ga_ourdjini@esi.dz>
2020-09-13 19:30:17 +0100

    logging in | logging out | checking authentication | updating accounts of users

--- src/account/forms.py
+++ src/account/forms.py
@@ -1,14 +1,62 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login,authenticate
+
 
 from account.models import Account
 
 
 
 class UserRegistrationForm(UserCreationForm):
+	#without this, there is no help text in the email field
 	email					= forms.EmailField(max_length=60, help_text="Required. Add a valid email address")
 
 	class Meta:
 		model = Account
 		fields = ('email','username','password1','password2')
+
+class AccountAuthentificationForm(forms.ModelForm):
+	#without this, the password field will be just a text field
+	password 				= forms.CharField(label='Password', widget=forms.PasswordInput)
+
+	class Meta:
+		model = Account
+		fields = ('email','password')
+
+
+	def clean(self):
+		""" this method is availabe in any form that extands the Model.form
+	    		it is like an interceptor,before the form can do anything it has to run it"""
+		if self.is_valid():
+			email = self.cleaned_data['email']
+			password = self.cleaned_data['password']
+			#if the email or the passoword is wrong , we must attach a validation error to the form
+			#this is a non-field error
+			if not authenticate(email=email,password=password):
+				raise forms.ValidationError("Invalid login")
+	
+
+class AccountUpdateForm(forms.ModelForm):
+	
+	class Meta:
+		model = Account
+		fields = ('email','username')
+
+	def clean_email(self):
+		if self.is_valid():
+			email = self.cleaned_data['email']
+			try:
+				account = Account.objects.exclude(pk=self.instance.pk).get(email=email)
+			except Account.DoesNotExist:
+				return email
+			raise forms.ValidationError("Email '%s' is alredy in use" % email)
+
+	def clean_username(self):
+		if self.is_valid():
+			username = self.cleaned_data['username']
+			try:
+				account = Account.objects.exclude(pk=self.instance.pk).get(username=username)
+			except Account.DoesNotExist:
+				return username
+			raise forms.ValidationError("Username '%s' is alredy in use" % username)
 		
--- src/account/views.py
+++ src/account/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth import login,authenticate
-from account.forms import UserRegistrationForm
+from django.contrib.auth import login,authenticate,logout
+from account.forms import UserRegistrationForm, AccountAuthentificationForm,AccountUpdateForm
 
 
 def registration_view(request):
@@ -8,10 +8,10 @@ def registration_view(request):
 	if request.POST:
 		form = UserRegistrationForm(request.POST)
 		if form.is_valid():
-			form.save()
+			form.save()#save the account
 			email = form.cleaned_data.get('email')
 			raw_password = form.cleaned_data.get('password1')
-			account = authenticate(email=email, password=raw_password)
+			account = authenticate(email=email, password=raw_password)#check the credentials
 			login(request,account)
 			return redirect('home')
 		else:
@@ -20,3 +20,48 @@ def registration_view(request):
 		form =UserRegistrationForm()
 		context['registration_form'] = form
 	return render(request,'account/register.html',context)
+
+def logout_view(request):
+	logout(request)
+	return redirect('home')
+
+def login_view(request):
+	context = {}
+	user = request.user
+	if user.is_authenticated:
+		return redirect('home')
+
+	if request.POST:
+		form = AccountAuthentificationForm(request.POST)
+		if form.is_valid():
+			email = request.POST['email']
+			password = request.POST['password']
+			user = authenticate(email=email,password=password)
+			if user:
+	 			login(request,user)
+	 			return redirect('home')
+	 
+	else:
+		form = AccountAuthentificationForm()
+	context['login_form'] = form
+	return render(request,'account/login.html',context)
+
+def account_view(request):
+	context = {}
+	if not request.user.is_authenticated:
+		return redirect('login')
+
+	if request.POST:
+		form = AccountUpdateForm(request.POST,instance=request.user)
+		if form.is_valid():
+			form.save()
+	else:
+		form = AccountUpdateForm(
+			initial = {
+				"email" : request.user.email,
+				"username" : request.user.username
+			}
+		)
+	context["account_form"] = form
+	return render(request,"account/account.html",context)
+
